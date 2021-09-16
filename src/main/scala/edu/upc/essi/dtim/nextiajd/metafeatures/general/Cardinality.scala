ebdfea892f7a993e700387ac47c974628af807b7
Javier Flores <javierfh1404@gmail.com>
2021-09-16 20:47:27 +0200

    license

--- src/main/scala/edu/upc/essi/dtim/nextiajd/metafeatures/general/Cardinality.scala
+++ src/main/scala/edu/upc/essi/dtim/nextiajd/metafeatures/general/Cardinality.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2020-2021 Javier de Jesus Flores Herrera, Sergi Nadal Francesch & Oscar Romero Moral
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ *
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.upc.essi.dtim.nextiajd.metafeatures.general
 
 import edu.upc.essi.dtim.nextiajd.metafeatures.{DefaultMF, MetaFeature}
