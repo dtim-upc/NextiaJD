7cea2cc6c43a7256040f3fb5f2ed05a44444c245
Sergi Nadal <snadal@essi.upc.edu>
2021-10-21 16:33:52 +0200

    fix output path

--- NextiaJDService/src/main/scala/nextiajd_api/ProfilingService.scala
+++ NextiaJDService/src/main/scala/nextiajd_api/ProfilingService.scala
@@ -2,15 +2,25 @@ package nextiajd_api
 
 import java.io.{File, PrintWriter}
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object ProfilingService {
 
+  //https://stackoverflow.com/a/41990859
+  def renamePartitionFile(spark: SparkSession, output: String): Unit = {
+    import org.apache.hadoop.fs._
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+    val file = fs.globStatus(new Path(output+"/part*"))(0).getPath().getName()
+
+    fs.rename(new Path(output+"/" +file), new Path(output+"/distances.json"))
+  }
+
   def profileFromCSV(spark: SparkSession, path: String, output: String): Unit = {
     import edu.upc.essi.dtim.NextiaJD.implicits
-    val DF = spark.read.csv(path)
+    val DF = spark.read.csv(path).cache()
     val profile = DF.attProfile()
-    profile.attProfile().showAttProfile.repartition(1).write.json(output)
+    profile.attProfile().showAttProfile.repartition(1).write.mode(SaveMode.Overwrite).json(output)
+    renamePartitionFile(spark,output)
   }
 
   def profileFromData(spark: SparkSession, data: String, output: String): Unit = {
@@ -38,10 +48,11 @@ object ProfilingService {
     import edu.upc.essi.dtim.NextiaJD.implicits
     import edu.upc.essi.dtim.nextiajd.Discovery
     val DF_A = spark.read.csv(pathA)
-    DF_A.show()
+    //DF_A.cache()
     val DF_B = spark.read.csv(pathB)
-    Discovery.preDist(DF_A,Seq(DF_B)).show()
-    Discovery.preDist(DF_A,Seq(DF_B)).repartition(1).write.json(output)
+    //DF_B.cache()
+    Discovery.preDist(DF_A,Seq(DF_B)).repartition(1).write.mode(SaveMode.Overwrite).json(output)
+    renamePartitionFile(spark,output)
   }
 
   def computeDistancesFromData(spark: SparkSession, dataA: String, dataB: String, output: String): Unit = {
